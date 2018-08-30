dc5da0b4052cc8056289446f8d0b79abcba7391f
Salcon77 <Salcon77@users.noreply.github.com>
2018-08-29 21:20:00 -0500

    x

---
+++ practice.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x=[]
+
+plt.scatter(x,y, label='scatter', color='k')
+
+
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.title('practice graph\nvars')
+plt.show()
